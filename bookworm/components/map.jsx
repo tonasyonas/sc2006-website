4907c057beffaa225102b18d00659f0cbd21eb63
JJChong777 <jiejun815@gmail.com>
2024-03-26 11:35:33 +0800

    change the map button

--- bookworm/components/map.jsx
+++ bookworm/components/map.jsx
@@ -30,7 +30,10 @@ function Map() {
   return (
     <div>
       {!geolocationPosition && (
-        <button onClick={getPosition}>
+        <button
+          onClick={getPosition}
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
           {isLoadingPosition ? "Loading..." : "Use your position"}
         </button>
       )}
