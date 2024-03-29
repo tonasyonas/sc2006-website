476593ca82fd5853c870bfd1052d051b59765fb7
Ding Ruoqian <bubuding0809@gmail.com>
2024-03-29 11:22:16 +0800

    Add rss-parser package and update librarydata route

--- bookworm/app/api/librarydata/route.js
+++ bookworm/app/api/librarydata/route.js
@@ -1,11 +1,24 @@
 import { NextResponse } from "next/server";
 
-const parser = require("rss-url-parser");
+let Parser = require("rss-parser");
+let parser = new Parser({
+  customFields: {
+    item: ["title", "address", "georss:point", "operatingHours"],
+  },
+});
 
 const RSSLibrariesURL = "https://eservice.nlb.gov.sg/rss/libraries";
 
+/**
+ * Retrieves library data from the RSS feed.
+ * @returns {Promise<NextResponse>} The response containing the library data.
+ */
 export async function GET() {
-  const res = await parser(RSSLibrariesURL);
-
-  return NextResponse.json(res);
+  try {
+    const feed = await parser.parseURL(RSSLibrariesURL);
+    return NextResponse.json(feed);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error(new Error("Failed to fetch library data"));
+  }
 }
